2425f5b18012f0e88ed7df5673d932a35a593dd9
alija-student <a.bickalo@students.htl-leonding.ac.at>
2026-05-28 19:46:26 +0200

    dark/light mode refined; style.css added

--- obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207c8bc2bfa10955b0d2ef18dbf0c953fa71677e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46e778cc9081f1f99f170d6a3cd40f6730d1ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
